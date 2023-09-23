0772043d0cf3c93cafb1d01c9b20ae7efdce7c74
T0nyBlue <18521123@gm.uit.edu.vn>
2023-09-23 21:02:30 +0700

    First commit, create project, add tailwind, add router, add axios

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col p-8">
+    <div>
+      <input
+        type="text"
+        class="rounded border-2 border-grey-200 w-full"
+        placeholder="Search for Meals"
+      />
+    </div>
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+        >{{ letter }}</router-link
+      >
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed, onMounted } from "vue";
+import store from "../store";
+import axiosClient from "../axiosClient";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+});
+</script>
